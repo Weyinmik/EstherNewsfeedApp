921fb54805806097c16ed11feeb1d1778a14e9cc
Esther Weyinmi Koloh <kolohw@gmail.com>
2018-08-04 11:42:38 +0200

    PART 2 OF NEWS APP - Initial commit

--- app/src/main/java/com/example/koloh/esthernewsfeedapp/MainActivity.java
+++ app/src/main/java/com/example/koloh/esthernewsfeedapp/MainActivity.java
@@ -10,6 +10,8 @@ import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.Adapter;
@@ -103,6 +105,7 @@ public class MainActivity extends AppCompatActivity implements android.app.Loade
         }
 
     }
+
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
@@ -199,6 +202,25 @@ public class MainActivity extends AppCompatActivity implements android.app.Loade
     @Override
     public void onLoaderReset(Loader<List<NewsFeedActivity>> loader) {
         loader.abandon ();
+    }
+
+    @Override
+    //This method initialiaze the content of the Activity's option menue
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Inflate the options Menu we specified in XML
+        getMenuInflater ().inflate ( R.menu.main, menu );
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId ();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent ( this, SettingsActivity.class );
+            startActivity ( settingsIntent );
+            return true;
+        }
+        return super.onOptionsItemSelected ( item );
     }
 }
 
