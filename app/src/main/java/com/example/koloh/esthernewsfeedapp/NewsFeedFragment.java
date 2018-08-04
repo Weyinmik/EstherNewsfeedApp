2890befaa27ea307a81f0e2df7aaa6d78671fc72
Esther Weyinmi Koloh <kolohw@gmail.com>
2018-08-05 00:54:42 +0200

    PART 2 OF NEWS APP - Java class Activity class updated

---
+++ app/src/main/java/com/example/koloh/esthernewsfeedapp/NewsFeedFragment.java
@@ -0,0 +1,255 @@
+package com.example.koloh.esthernewsfeedapp;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class NewsFeedFragment extends Fragment implements LoaderManager.LoaderCallbacks<List<NewsFeedActivity>> {
+    private static final String URL_KEY = "param1";
+    private View loadingView;
+    private TextView errorTextView;
+    private int loaderID = 0;
+    private SwipeRefreshLayout mySwipeRefreshLayout;
+    private String url;
+    private String page = "1";
+    private ArrayList<NewsFeedActivity> newsList = new ArrayList<> ();
+    private RecyclerAdapter adapter;
+    private RecyclerViewScrollListener scrollListener;
+    private RecyclerView recyclerView;
+
+    public NewsFeedFragment() {
+    }
+
+    public static NewsFeedFragment newInstance(String url) {
+        NewsFeedFragment fragment = new NewsFeedFragment ();
+        Bundle args = new Bundle ();
+        args.putString ( URL_KEY, url );
+        fragment.setArguments ( args );
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+        setRetainInstance ( true );
+        adapter = new RecyclerAdapter ( getContext (), newsList );
+        if (getArguments () != null) {
+            url = getArguments ().getString ( URL_KEY );
+        }
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate ( R.layout.news_fragment, container, false );
+        recyclerView = rootView.findViewById ( R.id.recycler_view );
+        loadingView = rootView.findViewById ( R.id.loading_linear_layout );
+        errorTextView = rootView.findViewById ( R.id.loading_error );
+        mySwipeRefreshLayout = rootView.findViewById ( R.id.swiperefresh );
+        if (isConnected () && newsList.isEmpty ()) {
+            getLoaderManager ().initLoader ( loaderID, null, this );
+            showLoading ();
+        } else if (errorTextView != null) {
+            errorTextView.setVisibility ( View.VISIBLE );
+            errorTextView.setText ( R.string.loading_fail_connection );
+        }
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager ( getContext (), LinearLayoutManager.VERTICAL, false );
+        recyclerView.setLayoutManager ( linearLayoutManager );
+        scrollListener = new RecyclerViewScrollListener ( linearLayoutManager ) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                loadNextDataFromApi ();
+            }
+        };
+        recyclerView.addOnScrollListener ( scrollListener );
+        recyclerView.setAdapter ( adapter );
+
+        return rootView;
+    }
+
+    public void loadNextDataFromApi() {
+        if (isConnected () && !newsList.isEmpty ()) {
+            int newPage = Integer.parseInt ( page );
+            page = String.valueOf ( ++newPage );
+            if (!newsList.contains ( null )) {
+                newsList.add ( null );
+                adapter.notifyItemInserted ( newsList.size () - 1 );
+            }
+            getLoaderManager ().restartLoader ( loaderID, null, this );
+        } else {
+            Toast.makeText ( getContext (), R.string.loading_fail_connection, Toast.LENGTH_SHORT ).show ();
+        }
+    }
+
+    @Override
+    public void setMenuVisibility(boolean menuVisible) {
+        super.setMenuVisibility ( menuVisible );
+        if (menuVisible) {
+            if (recyclerView != null && adapter != null) {
+                View view = recyclerView.getLayoutManager ().findViewByPosition ( 0 );
+                if (view != null) {
+                    ImageView imageView = view.findViewById ( R.id.main_image );
+                    imageView.startAnimation ( AnimationUtils.loadAnimation ( getContext (), R.anim.zoom ) );
+                }
+            }
+        } else {
+            if (recyclerView != null && adapter != null) {
+                View view = recyclerView.getLayoutManager ().findViewByPosition ( 0 );
+                if (view != null) {
+                    ImageView imageView = view.findViewById ( R.id.main_image );
+                    imageView.setAnimation ( null );
+                }
+            }
+        }
+    }
+
+    private void showLoading() {
+        loadingView.setVisibility ( View.VISIBLE );
+        errorTextView.setVisibility ( View.GONE );
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated ( view, savedInstanceState );
+        mySwipeRefreshLayout.setOnRefreshListener (
+                new SwipeRefreshLayout.OnRefreshListener () {
+                    @Override
+                    public void onRefresh() {
+                        refresh ();
+                    }
+                }
+        );
+        recyclerView.getViewTreeObserver ().addOnGlobalLayoutListener ( new ViewTreeObserver.OnGlobalLayoutListener () {
+            @SuppressLint("RestrictedApi")
+            @Override
+            public void onGlobalLayout() {
+                View view = recyclerView.getLayoutManager ().findViewByPosition ( 0 );
+                if (view != null) {
+                    ImageView imageView = view.findViewById ( R.id.main_image );
+
+                    //Set image animation to null
+                    if (isMenuVisible () && isVisible () && imageView.getTag () == null) {
+                        if (imageView.getAnimation () == null) {
+                            imageView.startAnimation ( AnimationUtils.loadAnimation ( getContext (), R.anim.zoom ) );
+
+                        } else {
+                            imageView.getAnimation ().cancel ();
+                        }
+                    }
+                }
+            }
+        } );
+
+    }
+
+    private void refresh() {
+        if (isConnected ()) {
+            newsList.clear ();
+            page = "1";
+            scrollListener.resetState ();
+            getLoaderManager ().restartLoader ( loaderID, null, this );
+        } else {
+            errorTextView.setText ( R.string.loading_fail_connection );
+            Toast.makeText ( getContext (), "No internet connection found", Toast.LENGTH_SHORT ).show ();
+            hideLoading ();
+            if (newsList.isEmpty ()) {
+                errorTextView.setVisibility ( View.VISIBLE );
+            } else {
+                errorTextView.setVisibility ( View.GONE );
+            }
+        }
+    }
+
+    private void hideLoading() {
+        if (mySwipeRefreshLayout.isRefreshing ()) {
+            mySwipeRefreshLayout.setRefreshing ( false );
+        }
+        loadingView.setVisibility ( View.GONE );
+    }
+
+    private boolean isConnected() {
+        if (getContext () != null) {
+            ConnectivityManager cm =
+                    (ConnectivityManager) getContext ().getSystemService ( Context.CONNECTIVITY_SERVICE );
+            NetworkInfo activeNetwork = null;
+            if (cm != null) {
+                activeNetwork = cm.getActiveNetworkInfo ();
+            }
+            return activeNetwork != null &&
+                    activeNetwork.isConnectedOrConnecting ();
+        }
+        return false;
+    }
+
+    @NonNull
+    @Override
+    public Loader<List<NewsFeedActivity>> onCreateLoader(int id, @Nullable Bundle args) {
+        return new NewsFeedLoader ( getContext (), NewsQueryUtils.createUrl ( uriParser ( url, page ) ) );
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<List<NewsFeedActivity>> loader, List<NewsFeedActivity> data) {
+        hideLoading ();
+        if (!data.isEmpty ()) {
+            if (!newsList.isEmpty () && newsList.contains ( null )) {
+                newsList.remove ( newsList.size () - 1 );
+                adapter.notifyItemRemoved ( newsList.size () - 1 );
+            }
+            newsList.addAll ( data );
+            adapter.notifyItemRangeInserted ( newsList.size (), data.size () );
+            errorTextView.setVisibility ( View.GONE );
+
+        } else {
+            if (newsList.isEmpty ()) {
+                errorTextView.setText ( R.string.home_screen_text ); // Default home screen message when there is internet
+                errorTextView.setVisibility ( View.VISIBLE );
+            }
+        }
+        View view = recyclerView.getLayoutManager ().findViewByPosition ( 0 );
+        if (view != null) {
+            ImageView imageView = view.findViewById ( R.id.main_image );
+            imageView.startAnimation ( AnimationUtils.loadAnimation ( getContext (), R.anim.zoom ) );
+
+        }
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<List<NewsFeedActivity>> loader) {
+
+    }
+
+    private String uriParser(String url, String page) {
+        Uri uri = Uri.parse ( url );
+        return uri.buildUpon ().appendQueryParameter ( "page", page ).toString ();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState ( outState );
+        getLoaderManager ().destroyLoader ( loaderID );
+    }
+
+}
---
+++ app/src/main/java/com/example/koloh/esthernewsfeedapp/RecyclerAdapter.java
@@ -0,0 +1,350 @@
+package com.example.koloh.esthernewsfeedapp;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.TextAppearanceSpan;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private static final int MAIN_ITEM = 0;
+    private static final int SIDE_ITEM = 1;
+    private static final int LOADING_ITEM = 2;
+    private static final int NO_DATA_FOUND = -1;
+    private Context mContext;
+    private List<NewsFeedActivity> newsList;
+    private OnSectionClickListener sectionClickListener;
+
+    RecyclerAdapter(Context mContext, List<NewsFeedActivity> newsList) {
+        this.mContext = mContext;
+        this.newsList = newsList;
+        try {
+            sectionClickListener = (OnSectionClickListener) mContext;
+        } catch (ClassCastException e) {
+            e.printStackTrace ();
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (!newsList.isEmpty ()) {
+            if (position == 0) {
+                return MAIN_ITEM;
+            } else {
+                return newsList.get ( position ) == null ? LOADING_ITEM : SIDE_ITEM;
+            }
+        } else
+            return NO_DATA_FOUND;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        switch (viewType) {
+            case MAIN_ITEM:
+                return new MainStoryHolder ( LayoutInflater.from ( mContext ).inflate ( R.layout.main_news, parent, false ) );
+            case SIDE_ITEM:
+                return new SideStoryHolder ( LayoutInflater.from ( mContext ).inflate ( R.layout.news_list_item, parent, false ) );
+            case LOADING_ITEM:
+                return new LoadingView ( LayoutInflater.from ( mContext ).inflate ( R.layout.loading_list, parent, false ) );
+            case NO_DATA_FOUND:
+                return new DummyHolder ( new View ( mContext ) );
+            default:
+                throw new IllegalArgumentException ( "Unexpected view type " + viewType );
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (holder.getItemViewType () == MAIN_ITEM) {
+            setMainItem ( (MainStoryHolder) holder, newsList.get ( position ) );
+        } else if (holder.getItemViewType () == SIDE_ITEM) {
+            setSideItem ( (SideStoryHolder) holder, newsList.get ( position ) );
+        }
+
+    }
+
+    private void setSideItem(SideStoryHolder holder, final NewsFeedActivity story) {
+        holder.container.setOnClickListener ( new View.OnClickListener () {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent ( Intent.ACTION_VIEW );
+                i.setData ( Uri.parse ( story.getWebUrl () ) );
+                PackageManager packageManager = mContext.getPackageManager ();
+                if (i.resolveActivity ( packageManager ) != null) {
+                    mContext.startActivity ( i );
+                } else {
+                    Toast.makeText ( mContext, R.string.browser_intent_error, Toast.LENGTH_SHORT ).show ();
+                }
+            }
+        } );
+        holder.section.setOnClickListener ( new View.OnClickListener () {
+            @Override
+            public void onClick(View v) {
+                sectionClickListener.onClick ( story.getSection () );
+            }
+        } );
+        String line = story.getTitle ();
+        if (!story.getAuthor ().equals ( "" )) {
+            line += "\n" + mContext.getResources ().getString ( R.string.by ) + " " + story.getAuthor ();
+        }
+        SpannableString text = new SpannableString ( line );
+        text.setSpan ( new TextAppearanceSpan ( mContext, R.style.title ), 0, story.getTitle ().length (), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE );
+        holder.title.setText ( text, TextView.BufferType.SPANNABLE );
+
+        holder.date.setText ( parseDate ( story.getDate () ) );
+        holder.section.setText ( story.getSection () );
+        if (story.getImageUrl ().equals ( "" )) {
+            holder.imageView.setImageResource ( R.drawable.absent_image );
+        } else {
+            Picasso.get ().load ( story.getImageUrl () ).error ( R.drawable.absent_image ).into ( holder.imageView );
+
+        }
+
+    }
+
+    private void setMainItem(final MainStoryHolder holder, final NewsFeedActivity story) {
+        holder.container.setOnClickListener ( new View.OnClickListener () {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent ( Intent.ACTION_VIEW );
+                i.setData ( Uri.parse ( story.getWebUrl () ) );
+                PackageManager packageManager = mContext.getPackageManager ();
+                if (i.resolveActivity ( packageManager ) != null) {
+                    mContext.startActivity ( i );
+                } else {
+                    Toast.makeText ( mContext, R.string.browser_intent_error, Toast.LENGTH_SHORT ).show ();
+                }
+            }
+        } );
+        holder.section.setOnClickListener ( new View.OnClickListener () {
+            @Override
+            public void onClick(View v) {
+                sectionClickListener.onClick ( story.getSection () );
+            }
+        } );
+        String line = story.getTitle ();
+        if (!story.getAuthor ().equals ( "" )) {
+            line += "\n" + mContext.getResources ().getString ( R.string.by ) + " " + story.getAuthor ();
+        }
+        SpannableString text = new SpannableString ( line );
+        text.setSpan ( new TextAppearanceSpan ( mContext, R.style.title_main ), 0, story.getTitle ().length (), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE );
+        holder.title.setText ( text, TextView.BufferType.SPANNABLE );
+
+        holder.date.setText ( parseDate ( story.getDate () ) );
+
+        holder.section.setText ( story.getSection () );
+        if (story.getImageUrl ().equals ( "" )) {
+            holder.imageView.setImageResource ( R.drawable.absent_image );
+        } else {
+            Picasso.get ().load ( story.getImageUrl () ).into ( holder.imageView );
+        }
+
+
+    }
+
+
+    private String parseDate(String date) {
+        if (date.equalsIgnoreCase ( "" )) {
+            return "";
+        }
+        SimpleDateFormat inputDate = new SimpleDateFormat ( "yyyy-MM-dd'T'HH:mm:ss'Z'" );
+        String timeAtMilliseconds = "";
+        try {
+            Date newDate = inputDate.parse ( date );
+            timeAtMilliseconds = String.valueOf ( newDate.getTime () );
+        } catch (ParseException e) {
+            e.printStackTrace ();
+        }
+        String result = "now";
+//        @SuppressLint("SimpleDateFormat") SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//        String todayDate = formatter.format(new Date());
+//        Calendar calendar = Calendar.getInstance();
+//
+        long dayAgoLong = Long.valueOf ( timeAtMilliseconds );
+//        calendar.setTimeInMillis(dayAgoLong);
+//        String agoFormatter = formatter.format(calendar.getTime());
+//
+//        Date CurrentDate ;
+//        Date CreateDate ;
+        int timeZoneDifference = Calendar.getInstance ().getTimeZone ().getRawOffset ();
+        try {
+//            CurrentDate = formatter.parse(todayDate);
+//            CreateDate = formatter.parse(agoFormatter);
+            long different = new Date ().getTime () - (dayAgoLong + timeZoneDifference);
+
+
+//            long different = Math.abs(CurrentDate.getTime() - CreateDate.getTime());
+
+            long secondsInMilli = 1000;
+            long minutesInMilli = secondsInMilli * 60;
+            long hoursInMilli = minutesInMilli * 60;
+            long daysInMilli = hoursInMilli * 24;
+
+            long elapsedDays = different / daysInMilli;
+            different = different - (elapsedDays * daysInMilli);
+
+            long elapsedHours = different / hoursInMilli;
+            different = different - (elapsedHours * hoursInMilli);
+
+            long elapsedMinutes = different / minutesInMilli;
+            different = different - (elapsedHours * hoursInMilli);
+
+            long elapsedSeconds = different / secondsInMilli;
+
+            if (elapsedDays == 0) {
+                if (elapsedHours == 0) {
+                    if (elapsedMinutes == 0) {
+                        if (elapsedSeconds < 0) {
+                            return "0" + " s";
+                        } else {
+                            return "now";
+
+                        }
+                    } else {
+                        return String.valueOf ( elapsedMinutes ) + "m ago";
+                    }
+                } else {
+                    return String.valueOf ( elapsedHours ) + "h ago";
+                }
+
+            } else {
+                if (elapsedDays <= 29) {
+                    return String.valueOf ( elapsedDays ) + "d ago";
+                }
+                if (elapsedDays <= 58) {
+                    return "1Mth ago";
+                }
+                if (elapsedDays <= 87) {
+                    return "2Mth ago";
+                }
+                if (elapsedDays <= 116) {
+                    return "3Mth ago";
+                }
+                if (elapsedDays <= 145) {
+                    return "4Mth ago";
+                }
+                if (elapsedDays <= 174) {
+                    return "5Mth ago";
+                }
+                if (elapsedDays <= 203) {
+                    return "6Mth ago";
+                }
+                if (elapsedDays <= 232) {
+                    return "7Mth ago";
+                }
+                if (elapsedDays <= 261) {
+                    return "8Mth ago";
+                }
+                if (elapsedDays <= 290) {
+                    return "9Mth ago";
+                }
+                if (elapsedDays <= 319) {
+                    return "10Mth ago";
+                }
+                if (elapsedDays <= 348) {
+                    return "11Mth ago";
+                }
+                if (elapsedDays <= 360) {
+                    return "12Mth ago";
+                }
+
+                if (elapsedDays <= 720) {
+                    return "1 year ago";
+                }
+
+                @SuppressLint("SimpleDateFormat")
+                SimpleDateFormat formatterYear = new SimpleDateFormat ( "MM/dd/yyyy" );
+                Calendar calendarYear = Calendar.getInstance ();
+                calendarYear.setTimeInMillis ( dayAgoLong );
+                return formatterYear.format ( calendarYear.getTime () ) + "";
+
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace ();
+        }
+        return result;
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return newsList.size ();
+    }
+
+    interface OnSectionClickListener {
+        void onClick(String title);
+    }
+
+    class MainStoryHolder extends RecyclerView.ViewHolder {
+        private TextView title;
+        private TextView section;
+        private TextView date;
+        private ImageView imageView;
+        private View container;
+
+        MainStoryHolder(View view) {
+            super ( view );
+            this.title = view.findViewById ( R.id.main_title );
+            this.section = view.findViewById ( R.id.main_section );
+            this.date = view.findViewById ( R.id.main_date );
+            this.imageView = view.findViewById ( R.id.main_image );
+            this.container = view.findViewById ( R.id.main_item );
+        }
+    }
+
+    class SideStoryHolder extends RecyclerView.ViewHolder {
+        public ImageView imageView;
+        private TextView title;
+        private TextView section;
+        private TextView date;
+        private View container;
+
+        SideStoryHolder(View view) {
+            super ( view );
+            this.title = view.findViewById ( R.id.title_textview );
+            this.section = view.findViewById ( R.id.section_textview );
+            this.date = view.findViewById ( R.id.date_textview );
+            this.container = view.findViewById ( R.id.side_item );
+            this.imageView = view.findViewById ( R.id.image_view );
+        }
+    }
+
+    class LoadingView extends RecyclerView.ViewHolder {
+        View loading;
+
+        LoadingView(View itemView) {
+            super ( itemView );
+            this.loading = itemView.findViewById ( R.id.loading_view );
+        }
+    }
+
+    class DummyHolder extends RecyclerView.ViewHolder {
+
+        DummyHolder(View itemView) {
+            super ( itemView );
+        }
+    }
+}
